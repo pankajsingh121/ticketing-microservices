c361fdc842b588550de8045e8c0b00b889c53157
pankajsingh121 <pankajbhadauriya121@gmail.com>
2020-08-05 23:41:46 +0530

    'order-cancelled'

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
-
 import { app } from './app';
-import {natsWrapper} from './nats-wrapper';
+import { natsWrapper } from './nats-wrapper';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
+
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
@@ -23,14 +25,18 @@ const start = async () => {
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID,
       process.env.NATS_CLIENT_ID,
-      process.env.NATS_URL,
-      );
+      process.env.NATS_URL
+    );
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
       process.exit();
     });
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
